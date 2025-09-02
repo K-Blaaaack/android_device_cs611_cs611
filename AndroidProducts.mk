c68a4aab4fa7f886bdad3de7043882a45a44b364
K-Black <75488232+K-Blaaaack@users.noreply.github.com>
2025-09-02 21:31:27 +0800

    update

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_CS611.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_CS611-user \
+    omni_CS611-userdebug \
+    omni_CS611-eng
---
+++ omni_CS611.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from CS611 device
+$(call inherit-product, device/cs611/CS611/device.mk)
+
+PRODUCT_DEVICE := CS611
+PRODUCT_NAME := omni_CS611
+PRODUCT_BRAND := CS611
+PRODUCT_MODEL := CS611
+PRODUCT_MANUFACTURER := cs611
+
+PRODUCT_GMS_CLIENTID_BASE := android-cs611
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_k39tv1_bsp-user 8.1.0 O11019 1607435351 release-keys"
+
+BUILD_FINGERPRINT := CS611/CS611/CS611:8.1.0/O11019/1607435351:user/release-keys
